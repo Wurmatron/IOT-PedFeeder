9925eb5f30758554f3f162630344e8ff535ab9ad
creiber1 <creiber1@emich.edu>
2021-03-16 16:09:43 -0400

    added pinning

---
+++ controller/src/main/java/io/wurmatron/petfeeder/gpio/Pinning.java
@@ -0,0 +1,21 @@
+package io.wurmatron.petfeeder.gpio;
+
+public class Pinning {
+
+    public static final int load_cell_vcc = 2;
+    public static final int load_cell_gnd = 6;
+    public static final int load_cell_sck = 16;
+    public static final int load_cell_dt = 18;
+
+    public static final int servo_pos = 4;
+    public static final int servo_neg = 14;
+    public static final int servo_pam = 13;
+
+    public static final int photo_vcc = 1;
+    public static final int photo_gnd = 9;
+    public static final int photo_D0 = 11;
+
+    public static final int led_pos = 40;
+    public static final int led_neg = 39;
+
+}
